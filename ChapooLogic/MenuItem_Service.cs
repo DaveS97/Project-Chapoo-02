6ecf35dcf1823c277b3fe4940162832ca8c09f50
Samuel-code-design <54932034+Samuel-code-design@users.noreply.github.com>
2020-05-17 16:40:29 +0200

    menuItem_Service aangepast

--- ChapooLogic/MenuItem_Service.cs
+++ ChapooLogic/MenuItem_Service.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChapooDAL;
+using ChapooModel;
+using MenuItem = ChapooModel.MenuItem;
 
 namespace ChapooLogic
 {
     public class MenuItem_Service
     {
+        private MenuItem_DAO menuItem = new MenuItem_DAO();
 
+        public List<MenuItem> GetMenuItems()
+        {
+            try
+            {
+                List<MenuItem> item = menuItem.GetMenuItems();
+                return item;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Chapoo couldn't connect to the database " + e.Message);
+                List<MenuItem> werknemers = new List<MenuItem>();
+                MenuItem item = new MenuItem();
+                item.ID = 1;
+                item.Aantal = 0;
+                item.ItemNaam = "menuItem";
+                item.Prijs = 1.0;
+
+                werknemers.Add(item);
+                return werknemers;
+            }
+        }
     }
 }
