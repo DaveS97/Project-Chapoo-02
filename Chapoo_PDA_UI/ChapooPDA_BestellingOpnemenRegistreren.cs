14f85d39ae28298019e03bdb9ca3c2d06d2d98c1
Davey Smit <davey.smit10@gmail.com>
2020-05-27 17:39:00 +0200

    add registratiemenu bestellingen opnemen

---
+++ Chapoo_PDA_UI/ChapooPDA_BestellingOpnemenRegistreren.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapoo_PDA_UI
+{
+    public partial class ChapooPDA_BestellingOpnemenRegistreren : Form
+    {
+        private int tafelnummer;
+
+        public ChapooPDA_BestellingOpnemenRegistreren(int tafelnummer)
+        {
+            this.tafelnummer = tafelnummer;
+            InitializeComponent();
+        }
+
+        private void ChapooPDA_BestellingOpnemenRegistreren_Load(object sender, EventArgs e)
+        {
+            ShowPanelOpnemen();
+        }
+
+        private void ShowPanelOpnemen()
+        {
+            pnlBestellingOpnemen.Show();
+            lblTafelnummer.Text = "Tafel " + tafelnummer;
+        }
+    }
+}
