45ed29cb120f28af0835e75436163e8c95bc0935
Davey Smit <davey.smit10@gmail.com>
2020-06-15 13:59:09 +0200

    Add rekeningItem

---
+++ ChapooDAL/RekeningItem_DAO.cs
@@ -0,0 +1,24 @@
+﻿using ChapooModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapooDAL
+{
+    public class RekeningItem_DAO : Base
+    {
+        public void Write_To_Db_RekeningItem(int rekeningID, int menuItemID)
+        {
+            string query = "INSERT INTO RekeningItem VALUES (@rekeningID, @menuItemID)";
+            SqlParameter[] sqlParameters =
+            {
+                new SqlParameter("@rekeningID", SqlDbType.Int) { Value = rekeningID},
+                new SqlParameter("@menuItemID", SqlDbType.Int) { Value = menuItemID}
+            };
+        }
+    }
+}
---
+++ ChapooLogic/RekeningItem_Service.cs
@@ -0,0 +1,27 @@
+﻿using ChapooDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChapooLogic
+{
+    public class RekeningItem_Service
+    {
+        RekeningItem_DAO rekeningItem = new RekeningItem_DAO();
+
+        public void Write_To_Db_RekeningItem(int rekeningID, int menuItemID)
+        {
+            try
+            {
+                rekeningItem.Write_To_Db_RekeningItem(rekeningID, menuItemID);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Chapoo couldn't connect to the database " + e.Message);
+            }
+        }
+    }
+}
